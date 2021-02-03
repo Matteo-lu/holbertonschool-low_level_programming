6b9696a562a5c6c579e4cd8e7ac6e3670ecc3505
Matteo-lu <mateolondono.u@gmail.com>
2021-02-03 22:29:07 +0000

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints some sentence
+ *
+ * Return: 0 before prints the sentence
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
