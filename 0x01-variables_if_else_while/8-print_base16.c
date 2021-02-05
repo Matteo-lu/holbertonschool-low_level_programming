a15080903b163e99bf0d8dfe7d3a3423af34bebf
Matteo-lu <mateolondono.u@gmail.com>
2021-02-05 04:33:13 +0000

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints some sentence
+ *
+ * Return: 0 before prints the sentence
+ */
+
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	for (num = 'a'; num <= 'f'; num ++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
