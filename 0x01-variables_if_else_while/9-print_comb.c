0c5cfaccf0fc3d6130b033e1f6d682e98cbdc2d2
Matteo-lu <mateolondono.u@gmail.com>
2021-02-05 04:58:09 +0000

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints some sentence
+ *
+ * Return: 0 before prints the sentence
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		if (num != '9')
+			putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
