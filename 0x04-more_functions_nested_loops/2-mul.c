49be02cf7b008a33f9eca618aeea3f133b387694
Matteo-lu <mateolondono.u@gmail.com>
2021-02-11 17:51:24 +0000

    2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * mul - multiples two integers
+ * @a: variable 1
+ * @b: variable 2
+ * Return: the operation
+ *
+ */
+
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+	return (mult);
+
+}
