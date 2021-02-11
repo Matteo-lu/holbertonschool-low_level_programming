1a6d1a55fc45a3e3550b0be958c998b14746ffc1
Matteo-lu <mateolondono.u@gmail.com>
2021-02-11 02:37:30 +0000

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * add - Prints the alphabet
+ * @f: variable 1
+ * @c: variable 2
+ * Return: 0 before prints a
+ *
+ */
+
+int add(int f, int c)
+{
+
+	int a;
+
+	a = f + c;
+	return (a);
+}
