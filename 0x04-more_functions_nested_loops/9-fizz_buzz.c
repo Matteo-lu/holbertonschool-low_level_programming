8444038a77bc1548ff4a9a20f84d0d97e1928380
Matteo-lu <mateolondono.u@gmail.com>
2021-02-12 22:57:26 +0000

    9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 99; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
+			printf(", ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+			printf(", ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+			printf(", ");
+		}
+		else
+		{
+			
+			printf("%d", i);
+			if (!(i == 99))
+			{
+			printf(", ");
+			}
+		}
+	}
+	printf("\n");
+	return(0);
+}
