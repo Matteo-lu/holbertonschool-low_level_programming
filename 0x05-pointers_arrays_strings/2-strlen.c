155f2c6d7115b96afaeefc4bc76f81040e5ba59c
Matteo-lu <mateolondono.u@gmail.com>
2021-02-16 23:28:39 +0000

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - Checks for a digit
+ *
+ * @s: Variable s
+ *
+ * Return: Always return 0
+ *
+ */
+
+int _strlen(char *s)
+{
+	int lenght;
+	lenght = strlen(s);
+	return (lenght);
+}
