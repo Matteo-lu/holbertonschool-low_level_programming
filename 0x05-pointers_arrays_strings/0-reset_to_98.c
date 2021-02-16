09fe9d81106cdeb3820445047e7c154993e15d07
Matteo-lu <mateolondono.u@gmail.com>
2021-02-16 19:20:43 +0000

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Checks for a digit
+ *
+ * @n: Pointer variable to int
+ * Return: 1 if c is a digit or 0 otherwise
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
