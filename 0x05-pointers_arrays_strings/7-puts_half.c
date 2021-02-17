17a0050cf5cecf16b97f264c613d71f655ae228d
Matteo-lu <mateolondono.u@gmail.com>
2021-02-17 03:32:16 +0000

    7. Ganar es solo la mitad. Divertirse es la otra mitad correction1

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,11 +12,15 @@
 void puts_half(char *str)
 {
 	int i;
-	int count;
+	int count = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
+	{
 		count++;
-
+	}
+	if (count % 2 != 0)
+		count--;
+	
 	for (i = count / 2; str[i] != '\0'; i++)
 	{
 		if (str[i] != '\0')
