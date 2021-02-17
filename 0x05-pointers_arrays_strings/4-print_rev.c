9ac356611a014113a8aac47c9ce96da21e454861
Matteo-lu <mateolondono.u@gmail.com>
2021-02-17 01:31:43 +0000

    4. I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_rev - Checks for a digit
+ *
+ * @s: Variable pointer string
+ *
+ * Return: Always return 0
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int count;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+
+	for (i = count; i > 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
