eeee74050bf3a370b3fe76117ee259929f0e624c
Matteo-lu <mateolondono.u@gmail.com>
2021-02-18 20:42:54 +0000

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strncpy - Concatenates two strings
+ *
+ * @dest: Variable pointer to array
+ * @src: Variable pointer to array
+ * @n: Variable integer
+ * Return: Always return 0
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		if (i < n)
+		{
+			dest[i] = src[i];
+		}
+	dest[i] = 0;
+	return (dest);
+}
