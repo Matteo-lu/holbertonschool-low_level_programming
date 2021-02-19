d12833d3f3ed6c2a41bd184f0d600903d46f0ee0
Matteo-lu <mateolondono.u@gmail.com>
2021-02-19 00:53:27 +0000

    4. I am a kind of paranoid in reverse.

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - Concatenates two strings
+ *
+ * @dest: Variable pointer to array
+ * @src: Variable pointer to array
+ * @n: Variable integer
+ * Return: Always return 0
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int aux[1000];
+
+
+	for (i = 0; i < n; i++)
+	{
+		aux[n - 1 -i] = a[i];
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		a[i] = aux[i];
+	}
+}
