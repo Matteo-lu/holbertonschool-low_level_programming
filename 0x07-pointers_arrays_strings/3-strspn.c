53c5db2935313c48046192832f9c13b7d97c09dc
Matteo-lu <mateolondono.u@gmail.com>
2021-02-23 01:40:59 +0000

    3. strspn correction2

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -17,7 +17,7 @@ unsigned int _strspn(char *s, char *accept)
 	int j;
 	unsigned int num = 0;
 
-	for (i = 0; s[i] != ' '; i++)
+	for (i = 0; s[i] != ' ' && s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
