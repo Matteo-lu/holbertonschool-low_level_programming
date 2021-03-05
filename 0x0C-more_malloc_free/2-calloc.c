bb5f37eba00fca632616858aab9641430966eaaa
Matteo-lu <mateolondono.u@gmail.com>
2021-03-05 02:18:31 +0000

    2. _calloc correction3

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *memory = 0;
+	char *memory;
 	unsigned int i;
 
 	if (nmemb == 0)
@@ -28,7 +28,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 	{
 		memory[i] = 0;
 	}
