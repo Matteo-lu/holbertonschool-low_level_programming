5fa85e21fd708aac53334a9e4a2d393b5f1c08a9
Matteo-lu <mateolondono.u@gmail.com>
2021-03-09 02:01:45 +0000

    0. Django

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_H
+#define dog_H
+
+/**
+ * struct dog - Dog information
+ * @name: First member, dog's name
+ * @age: Second member, dog's age
+ * @owner: Third member, dog's owner
+ *
+ * Description: Dog information
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
