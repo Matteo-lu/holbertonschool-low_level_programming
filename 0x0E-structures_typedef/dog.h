d603729553261d1e6369f36cb667304552b07095
Matteo-lu <mateolondono.u@gmail.com>
2021-03-09 19:07:51 +0000

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef dog_H
-#define dog_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - Dog information
@@ -15,4 +15,6 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
