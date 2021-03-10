f8cfbd99d0f116d012b93de3c2342fb989768bd4
Matteo-lu <mateolondono.u@gmail.com>
2021-03-10 04:01:20 +0000

    function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+
+/**
+ * free_dog - Dog information
+ *
+ *
+ *
+ * @d: Pointer to structure
+ *
+ * Return: Dog information
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+		{
+			free(d->name);
+		}
+		if (d->owner != NULL)
+		{
+			free(d->owner);
+		}
+		free(d);
+	}
+}
