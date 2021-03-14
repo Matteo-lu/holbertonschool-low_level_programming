c6355c14eeecac38a6c7aa9decaadfc45dc87e37
Matteo-lu <mateolondono.u@gmail.com>
2021-03-14 20:52:37 +0000

    Header file actualization

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,8 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
 
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
