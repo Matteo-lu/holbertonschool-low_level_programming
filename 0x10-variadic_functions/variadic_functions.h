397c27b4e26bc91f00467ae7b458a3f3719561ff
Matteo-lu <mateolondono.u@gmail.com>
2021-03-15 18:06:11 +0000

    Header file complete

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,9 +4,28 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+/**
+ * struct list - Short description
+ * @s: First member
+ * @f: Second member
+ *
+ * Description: Longer description
+ */
+
+typedef struct list
+{
+	char *s;
+	void (*f)();
+}struct_fun;
+
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void character(va_list parameters);
+void integer(va_list parameters);
+void flotante(va_list parameters);
+void string(va_list parameters);
 
 #endif
