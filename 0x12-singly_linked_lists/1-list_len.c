9eb13729dd9fc8feb48d04ea49def10ace98702f
Matteo-lu <mateolondono.u@gmail.com>
2021-03-19 23:48:26 +0000

    correction

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,12 +4,12 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list
+ * list_len - prints all the elements of a list_t list
  * @h: Pointer to lists
  *
  * Return: Integer
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	int i;
 
