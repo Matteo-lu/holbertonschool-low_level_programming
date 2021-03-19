a655e2848718a5080fc6f3818506d52ccf04fa59
Matteo-lu <mateolondono.u@gmail.com>
2021-03-19 22:52:59 +0000

    4. Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include "lists.h"
+
+/**
+ * free_list - prints all the elements of a list_t list
+ * @head: Pointer to first node
+ *
+ * Return: direction of head
+ */
+void free_list(list_t *head)
+{
+	list_t *sig;
+
+	while (head != NULL)
+	{
+		sig = head->next;
+		free(head->str);
+		free(head);
+		head = sig;
+	}
+}
