a3dc9588d8c00468b225443d870234e2c1e6ca41
Matteo-lu <mateolondono.u@gmail.com>
2021-03-23 19:34:26 +0000

    correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			for (i = 0; i < (idx - 1); i++)
 			{
 				tmp = tmp->next;
-				if (tmp == NULL)
+				if (tmp->next == NULL)
 				{
 					return (NULL);
 				}
