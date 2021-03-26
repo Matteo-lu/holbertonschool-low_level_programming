7c306dec2a0df0d2000411d5631c322b689a3e4f
Matteo-lu <mateolondono.u@gmail.com>
2021-03-26 20:29:17 +0000

    Decimal to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_binary -  prints the binary representation of a number
+ * @n: The number
+ *
+ * Return: binary string
+ */
+void print_binary(unsigned long int n)
+{
+	int y, z, i, j, x;
+
+	z = n;
+	y = 1;
+
+	for (j = 0; z >= 1; j++)
+	{
+		z = z >> y;
+	}
+	j = j - 1;
+	for (i = j; i >= 0; i--)
+	{
+		x = n >> i;
+		_putchar('0' + (x & 1));
+	}
+}
