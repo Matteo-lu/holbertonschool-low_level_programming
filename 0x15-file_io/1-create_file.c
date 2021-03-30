3b3e4a0a0604cc0980ed53cc6b24f819501d588b
Matteo-lu <mateolondono.u@gmail.com>
2021-03-30 01:51:16 +0000

    function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a file
+ * @filename: file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: On success 1.
+ * On error, -1 is returned
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int i, fd, size;
+	char *c;
+
+	for (i = 0; text_content[i] != '\0'; i++)
+	{
+	}
+
+	c = malloc(i * sizeof(char));
+	if (c == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd < 0)
+	{
+		return (-1);
+	}
+	size = write(fd, text_content, i);
+	if (size < 0)
+	{
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
