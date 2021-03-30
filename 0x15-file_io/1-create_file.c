3857e0bc606ea04f01c94a03c6fdb26b456f4084
Matteo-lu <mateolondono.u@gmail.com>
2021-03-30 02:34:02 +0000

    function that creates a file correction3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,16 +11,16 @@ int create_file(const char *filename, char *text_content)
 {
 	int i, fd, size;
 
-	if (text_content == NULL)
-	{
-		return (-1);
-	}
 	for (i = 0; text_content[i] != '\0'; i++)
 	{
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
 	{
 		return (-1);
 	}
