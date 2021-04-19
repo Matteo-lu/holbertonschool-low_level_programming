453c9751b54d439e2af0e9f601fd73d8bc86771e
Mateo Londoño <mateolondono.u@gmail.com>
2021-04-19 17:11:32 -0500

    Correction new line character at the end of the string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,7 +17,8 @@ char *cap_string(char *str)
 	{
 		for (j = 0; separators[j] != '\0'; j++)
 		{
-			if (str[i + 1] >= 97 && str[i + 1] <= 122 && str[i] == separators[j])
+			if (str[i + 1] >= 97 && str[i + 1] <= 122 &&
+			str[i] == separators[j] && str[i + 1] != '\0')
 			{
 				str[i + 1] = str[i + 1] - 32;
 			}
