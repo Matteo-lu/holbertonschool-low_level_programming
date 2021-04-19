ada9e828fa8e5e95b807a3cd24a11f49ee303dc0
Mateo Londoño <mateolondono.u@gmail.com>
2021-04-19 16:58:58 -0500

    Betty's Correction

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,24 +3,25 @@
 
 /**
  * cap_string - capitalizes all words of a string
- * @a: Variable pointer to string
+ * @str: array to be capitalize
  * Return: Always return 0
  *
  */
 char *cap_string(char *str)
 {
 	unsigned int i, j;
-   	char separators[] = {',', ';', '.', '!', '?', '(', ')', '{', '}', '\n', '\t', ' '};
-   
-   	for (i = 0; str[i] != '\0'; i++)
-   	{
-     	for (j = 0; separators[j] != '\0'; j++)
-     	{
-        	if (str[i + 1] >= 97 && str[i + 1] <= 122 && str[i] == separators[j])
-        	{	
-          		str[i + 1] = str[i + 1] - 32;
-        	}
-     	}
-   	}
+	char separators[] = {',', ';', '.', '!', '?', '(', ')',
+		'{', '}', '\n', '\t', ' '};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; separators[j] != '\0'; j++)
+		{
+			if (str[i + 1] >= 97 && str[i + 1] <= 122 && str[i] == separators[j])
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
+	}
 	return (str);
 }
