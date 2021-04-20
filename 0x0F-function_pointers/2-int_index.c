270908df6b4e5a2b79f3ed2a996aa11d23ec0f97
Mateo Londoño <mateolondono.u@gmail.com>
2021-04-20 17:29:26 -0500

    returns corrections

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,20 +14,31 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int value, i, j = 0;
 
+	if (size <= 0)
+	{
+		return (-1);
+	}
 	for (i = 0; i < size; i++)
 	{
 		if (array[i] == 98)
 		{
 			value = cmp(array[i]);
 			if (value == 1)
+			{
 				return (i);
+			}
+			if (j == 0)
+				return (-1);
 		}
 		if (array[i] == -98)
 		{
 			value = cmp(array[i]);
 			if (value == 1)
+			{
+
 				return (i);
+			}
 		}
 	}
-	return (j);
+	return (-1);
 }
