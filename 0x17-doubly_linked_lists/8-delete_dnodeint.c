705d45bea3987c057a9edd0f9db7a1530e5ac2e2
Mateo Londoño <mateolondono.u@gmail.com>
2021-05-01 15:35:25 -0500

    head previouse pointer to NULL correction

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -17,10 +17,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (index == 0)
 		{
 			*head = (*head)->next;
-			free(delete);
+			(*head->prev) = NULL;
 		}
 
-		if (index > 0)
+		else
 		{
 			for (i = 0; i < (index - 1); i++)
 			{
