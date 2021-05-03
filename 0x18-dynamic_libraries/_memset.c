bbd977575e3947baeaec9c93b666195247067d93
Mateo Londoño <mateolondono.u@gmail.com>
2021-05-03 15:21:49 -0500

    Upload repository with task 1

---
+++ 0x18-dynamic_libraries/_memset.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _memset - writes the character c to stdout
+ * @s: The character to print
+ * @b: -
+ * @n: -
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+
+}
---
+++ 0x18-dynamic_libraries/_strchr.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _strchr - writes the character c to stdout
+ * @c: The character to print
+ * @s: -
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_strchr(char *s, char c)
+{
+
+}
