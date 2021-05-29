01fd3ef6b117cb2c71003076f23bca63bb3e3aae
Mateo Londoño <mateolondono.u@gmail.com>
2021-05-28 22:43:52 -0500

    Uploading task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table.
+ *
+ * @ht: The hash table
+ *
+ * Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int index = 0;
+	hash_node_t *run = NULL;
+
+	while (index < ht->size)
+	{
+		if (ht->array[index] != NULL)
+		{
+			run = ht->array[index];
+			while (run != NULL)
+			{
+				ht->array[index] = run->next;
+				free(run->key);
+				free(run->value);
+				free(run);
+				run = ht->array[index];
+			}
+		}
+		index++;
+	}
+	free(ht->array);
+	free(ht);
+}
