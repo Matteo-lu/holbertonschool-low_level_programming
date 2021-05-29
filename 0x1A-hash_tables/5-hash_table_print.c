cc85de6fe78d9dc8e60c3c48ae16d10e215ee2eb
Mateo Londoño <mateolondono.u@gmail.com>
2021-05-28 21:17:42 -0500

    Uploading task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table.
+ *
+ * @ht: The hash table
+ *
+ * Return: nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int index = 0;
+	hash_node_t *run = NULL;
+	int item = 0;
+
+	if (ht != NULL)
+	{
+		printf("{");
+		while (index < ht->size)
+		{
+			if (ht->array[index] != NULL)
+			{
+				run = ht->array[index];
+				while (run != NULL)
+				{
+					if (run->key != NULL)
+					{
+						if (item == 1)
+						{
+							printf(", ");
+						}
+						printf("\'%s\': \'%s\'", run->key, run->value);
+					}
+					item = 1;
+					run = run->next;
+				}
+			}
+			index++;
+		}
+		printf("}\n");
+	}
+}
