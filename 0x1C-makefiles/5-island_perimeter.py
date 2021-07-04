c3a91ab4a814264304fd01764cb7307051aa03f9
Mateo Londoño <mateolondono.u@gmail.com>
2021-07-04 09:28:51 -0500

    double land correction

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -22,10 +22,15 @@ def island_perimeter(grid):
                     add = add + grid[i + 1][j]
                 if 0 <= (i - 1) < len(grid):
                     add = add + grid[i - 1][j]
+
                 if add == 0 and count != 0:
                     perimeter = perimeter + 4
                 if add == 1:
                     perimeter = perimeter + 3
                 if add == 2:
                     perimeter = perimeter + 2
+                if add == 3:
+                    perimeter = perimeter + 1
+                if add == 4:
+                    perimeter = perimeter + 0
     return(perimeter)
