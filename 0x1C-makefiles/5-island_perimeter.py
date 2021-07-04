ee2d96419b8e938325f9fc236afa573204f04dfc
Mateo Londoño <mateolondono.u@gmail.com>
2021-07-03 20:34:48 -0500

    Uploading task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Technical interview preparation """
+
+
+def island_perimeter(grid):
+    """ Function that returns the perimeter of the island
+    described in grid"""
+
+    perimeter = 0
+    add = 0
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                add = 0
+                count = count + 1
+                if 0 <= (j + 1) < len(grid[i]):
+                    add = add + grid[i][j + 1]
+                if 0 <= (j - 1) < len(grid[i]):
+                    add = add + grid[i][j - 1]
+                if 0 <= (i + 1) < len(grid):
+                    add = add + grid[i + 1][j]
+                if 0 <= (i - 1) < len(grid):
+                    add = add + grid[i - 1][j]
+                if add == 0 and count != 0:
+                    perimeter = perimeter + 4
+                if add == 1:
+                    perimeter = perimeter + 3
+                if add == 2:
+                    perimeter = perimeter + 2
+    return(perimeter)
