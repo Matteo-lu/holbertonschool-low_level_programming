f0dea55cdfe30cb1b60fafc06c86ab5d9148d378
Matteo-lu <mateolondono.u@gmail.com>
2021-08-30 18:07:46 -0500

    putchar print correction

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,5 +3,7 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void int_put(int n);
+void print_values(unsigned int i, int value);
 
 #endif
