1487ecf9d99928c665457b34684b8268dd384281
Matteo-lu <mateolondono.u@gmail.com>
2021-09-01 14:28:27 -0500

    Updatig header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,5 +5,7 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void int_put(int n);
 void print_values(unsigned int i, int value);
+int jump_search(int *array, size_t size, int value);
+void print_prhase(unsigned int i, int value, char *prhase);
 
 #endif
